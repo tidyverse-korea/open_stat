62161ce5f03af86934bf7b1b663c0954b7478de3
Kwangchun Lee <kwangchun.lee.7@gmail.com>
2021-08-13 13:01:38 +0900

    seperate app.R in other words modulization ^^;

---
+++ global.R
@@ -0,0 +1,9 @@
+# install.packages("tidyverse")
+# install.packages("devtools")
+# devtools::install_github("tidyverse-korea/statdata")
+library(tidyverse)
+library(statdata)
+
+statdata::shadow_length
+statdata::gender
+
