b0364a596447db28d6153b57fdbfef0d38080560
Kwnaghcun Lee <kwangchun.lee.7@gmail.com>
2021-09-05 14:58:10 +0900

    added layout for gender dataset

--- global.R
+++ global.R
@@ -45,64 +45,6 @@ makePage <- function (title, subtitle, contents) {
 gender <- read_csv("www/025원시_성별.csv") %>% 
     set_names("성별")
 
-# gender %>% 
-#     filter(성별 == "남")
 
-gender_gt <- gender %>% 
-    gt::gt()
 
-# 2. Descriptive Statistics ----
 
-gender_stat_gt <- gender %>% 
-    count(성별, name = "명수") %>% 
-    mutate(성비 = glue::glue("{명수 / sum(명수) * 100} %")) %>% 
-    gt::gt()
-
-# 3. Visualization -----
-
-gender_tbl <- gender %>%
-    count(성별, name = "명수")
-
-# gender_barplot_gg <- gender_tbl %>% 
-#     ggplot(aes(x = 성별, y=명수)) +
-#     geom_col(width = 0.3, fill = "midnightblue") +
-#     scale_y_continuous(limits = c(0,10), labels = scales::number_format(accuracy = 1)) +
-#     labs(x = "성별",
-#          y = "명수",
-#          title = "중학교 성별 범주형 데이터") +
-#     theme_bw(base_family = "NanumGothic")
-
-# gender_piechart_gg <- gender_tbl %>% 
-#     ggplot(aes(x = "", y=명수, fill = 성별)) +
-#     geom_bar(width = 1, stat = "identity", color = "white") +
-#     coord_polar("y", start = 0) +
-#     geom_text(aes(label = glue::glue("{성별} : {명수}")), 
-#               position = position_stack(vjust = 0.5), 
-#               family = "NanumGothic",
-#               size = 10) +
-#     theme_void(base_family = "NanumGothic") +
-#     scale_fill_viridis_d() +
-#     theme(legend.position = "bottom") +
-#     labs(title = "중학교 성별 범주형 데이터")
-
-# gender_waffle_gg <- gender_tbl %>% 
-#     ggplot(aes(fill = 성별, values=명수)) +
-#     geom_waffle(n_rows = 6, size = 0.33, colour = "white") +
-#     scale_fill_manual(name = NULL,
-#                       values = c("#BA182A", "#FF8288"),
-#                       labels = c("남자", "여자")) +
-#     coord_equal() +
-#     theme_void(base_family = "NanumGothic")
-
-# gender_image_gg <- gender_tbl %>% 
-#     mutate(image = list(
-#         magick::image_read_svg("https://raw.githubusercontent.com/tidyverse-korea/pkg_doc/master/fig/man-svgrepo-com.svg"),
-#         magick::image_read_svg("https://raw.githubusercontent.com/tidyverse-korea/pkg_doc/master/fig/woman-svgrepo-com.svg")
-#     )) %>% 
-#     ggplot(aes(x = 성별, y=명수, image = image)) +
-#     geom_isotype_col() +
-#     scale_fill_manual(name = NULL,
-#                       values = c("#BA182A", "#FF8288"),
-#                       labels = c("남자", "여자")) +
-#     theme_bw(base_family = "NanumGothic")  +
-#     scale_y_continuous(limits = c(0,10), labels = scales::number_format(accuracy = 1))
--- ui.R
+++ ui.R
@@ -6,18 +6,86 @@ filters <- Stack(
     Toggle.shinyInput("maleOnly", value = TRUE, label = "남성만?")
 )
 
+# analysis 페이지 ----------------------------------------------------------
+
+analysis_page <- makePage(
+    "단변량 데이터셋",
+    "범주형 변수",
+    div(
+        Stack(
+            tokens = list(childrenGap = 10), horizontal = TRUE,
+            makeCard("데이터 탐색", filters, size = 4, style = "max-height: 320px"),
+            makeCard("데이터셋", gt_output(outputId = "datasetId"), size = 8, style = "max-height: 320px; overflow: auto")
+        ),
+        uiOutput("visualization"),
+        Stack(
+            tokens = list(childrenGap = 10), horizontal = TRUE,
+            makeCard("기술통계", gt_output(outputId = "gender_stat_gt"), size = 8, style = "max-height: 320px")
+        )
+    )
+)
+
+# UI Structure - Layout ----------------------------------------------------------
+
+header <- tagList(
+    img(src = "logo.jpg", width = "100px", class = "logo"),
+    div(Text(variant = "xLarge", "오픈 통계 팩키지"), class = "title"),
+    CommandBar(
+        items = list(
+            CommandBarItem("초등", "Upload"),
+            CommandBarItem("중등", "Upload"),
+            CommandBarItem("고등", "Upload")
+        ),
+        farItems = list(
+            CommandBarItem("Grid view", "Tiles", iconOnly = TRUE),
+            CommandBarItem("Info", "Info", iconOnly = TRUE)
+        ),
+        style = list(width = "100%")))
+
+navigation <- navigation <- Nav(
+    groups = list(
+        list(links = list(
+            list(name = 'Home', url = '#!/', key = 'home', icon = 'Home'),
+            list(name = '범주형', url = '#!/other', key = 'analysis', icon = 'AnalyticsReport'),
+            list(name = 'bit2r', url = 'https://r2bit.com/', key = 'bit2r', icon = 'GitGraph'),
+            list(name = 'R 컨퍼런스', url = 'http://use-r.kr', key = 'rconf', icon = 'WebAppBuilderFragment')
+        ))
+    ),
+    initialSelectedKey = 'home',
+    styles = list(
+        root = list(
+            height = '100%',
+            boxSizing = 'border-box',
+            overflowY = 'auto'
+        )
+    )
+)
+
+footer <- Stack(
+    horizontal = TRUE,
+    horizontalAlign = 'space-between',
+    tokens = list(childrenGap = 20),
+    Text(variant = "medium", "Built with Shiny and Fluent UI", block=TRUE),
+    Text(variant = "medium", nowrap = FALSE, "If you'd like to learn more, reach out to us at admin@r2bit.com"),
+    Text(variant = "medium", nowrap = FALSE, "All rights reserved.")
+)
+
+layout <- function(mainUI){
+    div(class = "grid-container",
+        div(class = "header", header),
+        div(class = "sidenav", navigation),
+        div(class = "main", mainUI),
+        div(class = "footer", footer)
+    )
+}
+
+
 # UI 페이지 ----------------------------------------------------------
 
 ui <- fluentPage(
     tags$style(".card { padding: 28px; margin-bottom: 28px; }"),
-    Stack(
-        tokens = list(childrenGap = 10), horizontal = TRUE,
-        makeCard("데이터 탐색", filters, size = 4, style = "max-height: 320px"),
-        makeCard("데이터셋", gt_output(outputId = "datasetId"), size = 8, style = "max-height: 320px; overflow: auto")
-    ),
-    uiOutput("visualization"),
-    Stack(
-        tokens = list(childrenGap = 10), horizontal = TRUE,
-        makeCard("기술통계", gt_output(outputId = "gender_stat_gt"), size = 8, style = "max-height: 320px")
-    )
+    layout(analysis_page),
+    tags$head(
+        tags$link(href = "style.css", rel = "stylesheet", type = "text/css"))
 )
+
